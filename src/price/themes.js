7266ba6cb1db6eeded2731520d4ac8c699103c6e
Aleksey <aleksey.amelchenko@gmail.com>
2020-12-22 17:42:20 +0300

    Initialize project using Create React App

---
+++ src/price/themes.js
@@ -0,0 +1,18 @@
+export const priceTableTheme = {
+    blue: {
+        verticalPadding: '50px 20px',
+        bgcContainer: '#3e98a1',
+        title: '#fff',
+        specificText: '#fff',
+        titleDownloadingText: '#fff',
+        iconContColor: '#fff',
+    },
+    normal: {
+        heightCenter: '500px',
+        bgcContainer: '#fff',
+        title: '#777777',
+        specificText: '#b1b1b2',
+        titleDownloadingText: '#b1b1b2',
+        iconContColor: '#8a8a8c',
+    },
+}
