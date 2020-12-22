0fc5e19325921a4839a4f30c90a825cecbccffb8
Aleksey <aleksey.amelchenko@gmail.com>
2020-12-22 17:42:51 +0300

    Initialize project using Create React App

---
+++ src/price/const.js
@@ -0,0 +1,32 @@
+export const plans = [
+  {
+    title: "Basic Plan",
+    specific: "This is Photoshop's version",
+    price: "25",
+    titleScore: "Number of downloads",
+    numberScore: "30",
+    numberScoreVideo: "YES",
+    numberScoreSystem: "ANDROID",
+    buttonName: "Select Plan"
+  },
+  {
+    title: "Pro Plan",
+    specific: "This is Photoshop's version",
+    price: "59",
+    titleScore: "Number of downloads",
+    numberScore: "60",
+    numberScoreVideo: "YES",
+    numberScoreSystem: "ANDROID",
+    buttonName: "Select Plan"
+  },
+  {
+    title: "Premium Plan",
+    specific: "This is Photoshop's version",
+    price: "99",
+    titleScore: "Number of downloads",
+    numberScore: "120",
+    numberScoreVideo: "YES",
+    numberScoreSystem: "ANDROID",
+    buttonName: "Select Plan"
+  }
+];
